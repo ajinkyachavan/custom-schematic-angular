935ca5bd61a7589e5975bc1a9f11eb598b0264ff
ajinkyachavan <a.ajinkya9030@gmail.com>
2022-09-20 17:13:21 -0400

    first commit

---
+++ src/hello-world/index.ts
@@ -0,0 +1,23 @@
+import { strings } from '@angular-devkit/core';
+import { apply, mergeWith, Rule, SchematicContext, template, Tree, url } from '@angular-devkit/schematics';
+import { Schema } from './schema';
+
+
+// You don't have to export the function as default. You can also have more than one rule factory
+// per file.
+export function helloWorld(_options: Schema): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+
+    const sourceTemplates = url('./files');
+
+    const sourceParameterizedTemplates = apply(sourceTemplates, [
+      template({
+        ..._options,
+        ...strings
+      })
+    ])
+
+
+    return mergeWith(sourceParameterizedTemplates)(tree, _context);
+  };
+}
